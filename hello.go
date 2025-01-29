64738f79af46034dad460870839308dafd7c6bb1
Gabriel Gambôa <gabriel.gamboa@mblabs.com.br>
2025-01-29 20:56:39 -0300

    feat: create a Hello function with some tests

--- hello.go
+++ hello.go
@@ -2,12 +2,38 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	spanish = "Spanish"
+	french  = "French"
 
-func Hello(name string) string {
-	return englishHelloPrefix + name
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+//named return value: create a variable called "prefix" in my function
+//Values are initialized with its "zero" values (0, "", false)
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,37 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Gambôa", "Spanish")
+		want := "Hola, Gambôa"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Gambôa", "French")
+		want := "Bonjour, Gambôa"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	//tell that method is a helper, and error will indicate the line that was called in the test
+	//ex: error is not on line 26, but in line 17 or 10
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
