4ca0f891969624805e8e99149f145d31f418c6b3
Gabrie Gambôa <gabriel_gamboa10@hotmail.com>
2025-02-03 10:39:20 -0300

    feat: learn slices

--- arrays_and_slices/sum.go
+++ arrays_and_slices/sum.go
@@ -1,6 +1,6 @@
 package arraysandslices
 
-func Sum(numbers [4]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 
 	for _, number := range numbers {
@@ -8,4 +8,25 @@ func Sum(numbers [4]int) int {
 	}
 
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
+
+	return sums
 }
--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -1,14 +1,48 @@
 package arraysandslices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [4]int{20, 10, 5, 5}
+	t.Run("collection of 4 values", func(t *testing.T) {
+		numbers := []int{20, 10, 5, 5}
 
-	response := Sum(numbers)
-	expected := 40
+		response := Sum(numbers)
+		expected := 40
 
-	if response != expected {
-		t.Errorf("expected %d but got %d. Numbers: %v", expected, response, numbers)
+		if response != expected {
+			t.Errorf("expected %d but got %d. Numbers: %v", expected, response, numbers)
+		}
+	})
+
+	t.Run("collection of any values", func(t *testing.T) {
+		numbers := []int{20, 10, 5, 5, 10, 50}
+
+		response := Sum(numbers)
+		expected := 100
+
+		if response != expected {
+			t.Errorf("expected %d but got %d. Numbers: %v", expected, response, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) { //DeepEqual is not type safe
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	got := SumAllTails([]int{1, 2, 3}, []int{0, 9})
+	want := []int{5, 9}
+
+	if !reflect.DeepEqual(got, want) { //DeepEqual is not type safe
+		t.Errorf("got %v want %v", got, want)
 	}
 }
